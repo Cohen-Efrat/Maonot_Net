04a43561c8779b1b1a46cb3e105503a291bf48a9
אפרת כהן <efratc66@gmail.com>
2018-08-27 20:58:07 +0300

    b

--- Maonot_Net/Controllers/FileUploadController.cs
+++ Maonot_Net/Controllers/FileUploadController.cs
@@ -16,6 +16,7 @@ namespace Maonot_Net.Controllers
             return View();
         }
         [HttpPost]
+        //שיקסלת רשימה של אי כםרצ ויעבור על הרשימה ויעשה את מה שצריך
         public async Task<IActionResult> UploadFile(IFormFile file)
         {
             if (file == null || file.Length == 0)
