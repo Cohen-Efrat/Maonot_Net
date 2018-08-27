10a29d78a8bf2fc07c79703af78cb624d5ea6e9e
אפרת כהן <efratc66@gmail.com>
2018-08-27 16:52:10 +0300

    g

---
+++ Maonot_Net/Models/Cloudinary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+namespace Maonot_Net.Models
+{
+
+
+        Account account = new Account(
+          "my_cloud_name",
+          "my_api_key",
+          "my_api_secret");
+
+        Cloudinary cloudinary = new Cloudinary(account);
+
+
+
+}
